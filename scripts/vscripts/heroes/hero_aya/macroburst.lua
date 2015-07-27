90c79f3956912923d3283b9d684a524a737d035e
Ethan Feinberg <sneakcow@gmail.com>
2015-07-26 19:33:49 -0700

    macroburst tweaks

--- scripts/vscripts/heroes/hero_aya/macroburst.lua
+++ scripts/vscripts/heroes/hero_aya/macroburst.lua
@@ -5,42 +5,39 @@ function fireGust( keys )
 	local caster = keys.caster
 	local ability = keys.ability
 	local particle = keys.particle
+	local ability_level = ability:GetLevel() - 1
 
-	local unitsNearTarget = FindUnitsInRadius(caster:GetTeamNumber(),
-                            caster:GetAbsOrigin(),
-                            nil,
-                            ability:GetLevelSpecialValueFor("range", (ability:GetLevel() - 1)),
-                            DOTA_UNIT_TARGET_TEAM_ENEMY,
-                            DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO,
-                            DOTA_UNIT_TARGET_FLAG_NO_INVIS,
-                            FIND_CLOSEST,
-                            false)
+	local team = caster:GetTeamNumber()
+	local radius = ability:GetLevelSpecialValueFor("range", ability_level)
+	local iTeam = DOTA_UNIT_TARGET_TEAM_ENEMY
+	local iType = DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO
+	local iFlag = DOTA_UNIT_TARGET_FLAG_NO_INVIS
+	local iOrder = FIND_CLOSEST
 
-	local target = unitsNearTarget[1]
-
-	if target ~= nil then
+	local units = FindUnitsInRadius(team, caster:GetAbsOrigin(), nil, radius, iTeam, iType, iFlag, iOrder, false)
+	if #units > 0 then
 		local projectile_info = {
-	        Target = target,
+	        Target = units[RandomInt(1, #units)],
 	        Source = caster,
 	        EffectName = particle,
 	        Ability = ability,
 	        bDodgeable = false,
 	        bProvidesVision = true,
-	        iMoveSpeed = ability:GetLevelSpecialValueFor("projectile_speed", (ability:GetLevel() - 1)),
+	        iMoveSpeed = ability:GetLevelSpecialValueFor("projectile_speed", ability_level),
 	        iVisionRadius = 0,
 	        iVisionTeamNumber = caster:GetTeamNumber(),
 	        iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1
 	    }
 
 	    ProjectileManager:CreateTrackingProjectile( projectile_info )
-	    distance_remaining_table[caster] = ability:GetLevelSpecialValueFor("distance_to_ready", (ability:GetLevel() - 1))
-		caster:RemoveModifierByName(keys.ready_modifier)
+	    distance_remaining_table[caster] = ability:GetLevelSpecialValueFor("distance_to_ready", ability_level)
 	end
 end
 
 function macroburstUpdateCooldown( keys )
 	local caster = keys.caster
 	local ability = keys.ability
+	local ability_level = ability:GetLevel() - 1
 
 	currentLocation = caster:GetAbsOrigin()
 	if previous_location_table[caster] == nil then
@@ -56,8 +53,6 @@ function macroburstUpdateCooldown( keys )
 		distance_remaining_table[caster] = distance_remaining_table[caster] - distance_traveled
 
 		if distance_remaining_table[caster] <= 0 then
-			ability:ApplyDataDrivenModifier(caster, caster, keys.ready_modifier, {})
-			-- fireGust(keys)
 			ability:EndCooldown()
 		else
 			ability:EndCooldown()
@@ -66,6 +61,21 @@ function macroburstUpdateCooldown( keys )
 	else
 		distance_remaining_table[caster] = nil
 		previous_location_table[caster] = nil
+	end
+
+	local team = caster:GetTeamNumber()
+	local radius = ability:GetLevelSpecialValueFor("range", ability_level)
+	local iTeam = DOTA_UNIT_TARGET_TEAM_ENEMY
+	local iType = DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO
+	local iFlag = DOTA_UNIT_TARGET_FLAG_NO_INVIS
+	local iOrder = FIND_CLOSEST
+
+	local units = FindUnitsInRadius(team, caster:GetAbsOrigin(), nil, radius, iTeam, iType, iFlag, iOrder, false)
+
+	if #units > 0 then
+		keys.ability:SetActivated(true)
+	else
+		keys.ability:SetActivated(false)
 	end
 end
 
